c8a5285a90c47faaacf9224f8cfd1ed0c25d28c8
dz20laza <Dominic.Zeiner@stud.hszg.de>
2025-02-27 17:40:36 +0100

    added editing own rating

--- src/main/java/com/example/watch_list/model/mapper/WatchlistMapper.java
+++ src/main/java/com/example/watch_list/model/mapper/WatchlistMapper.java
@@ -1,13 +1,13 @@
 package com.example.watch_list.model.mapper;
 
 import com.example.watch_list.model.entities.WatchlistEntity;
-import com.example.watch_list.model.entries.WatchListEntry;
+import com.example.watch_list.model.entries.WatchlistEntry;
 import org.springframework.stereotype.Component;
 
 @Component
 public class WatchlistMapper {
-    public WatchListEntry toWatchlistEntry(WatchlistEntity watchlistEntity) {
-        return new WatchListEntry(
+    public WatchlistEntry toWatchlistEntry(WatchlistEntity watchlistEntity) {
+        return new WatchlistEntry(
                 watchlistEntity.getMediaEntity().getImdbId(),
                 watchlistEntity.getStatus(),
                 watchlistEntity.getDateWatched(),
